1b708bbd79c0c2fa1f79e9fbcf9cb316f87d6439
Zaidan <zaidansz26@gmail.com>
2026-05-09 13:39:55 +0700

    First Commit

---
+++ MOD10_SE-48-01_FYI/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace MOD10_SE_48_01_FYI
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
